df5ed4326cff6309eccac4da7dab64bab7a06544
Alexander Nestorov <alexandernst@gmail.com>
2019-08-20 12:31:20 +0200

    Initial commit

---
+++ conf/autorun.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# WARNING: This script will be executed as root.
+# If you want to run it as another user, use su.
+
+#cd /var/www/webapp/src
+
+# Maybe install some dependencies?
+#pip3 install -r requirements.txt
+
+# Maybe collect statics?
+#python3 manage.py collectstatic --no-input
+
+# Maybe run migrations?
+# python3 manage.py migrate
