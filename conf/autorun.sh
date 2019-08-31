098121b298dbbc3c7900ef5d4f716fe8a3a0af5c
Alexander Nestorov <alexandernst@gmail.com>
2019-08-31 18:34:45 +0200

    Stop using sudo for pip install

--- conf/autorun.sh
+++ conf/autorun.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # WARNING: This script will be executed as the "admin" user.
-# If you want to run it as another user, use su.
+# If you want to run it as another user, use su/sudo.
 
 cd /var/www/webapp/src
 
 # Install some dependencies
-sudo -H -u www-data bash -c "pip3 install -r requirements.txt"
+pip3 install -r requirements.txt
 
 # Maybe collect statics?
 #python3 manage.py collectstatic --no-input
