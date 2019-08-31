da1c711e2d61b078b186428c419b191782295b7c
Alexander Nestorov <alexandernst@gmail.com>
2019-08-31 17:10:09 +0200

    Start installing the requirements

--- conf/autorun.sh
+++ conf/autorun.sh
@@ -3,10 +3,10 @@
 # WARNING: This script will be executed as the "admin" user.
 # If you want to run it as another user, use su.
 
-#cd /var/www/webapp/src
+cd /var/www/webapp/src
 
-# Maybe install some dependencies?
-#pip3 install -r requirements.txt
+# Install some dependencies
+pip3 install -r requirements.txt
 
 # Maybe collect statics?
 #python3 manage.py collectstatic --no-input
