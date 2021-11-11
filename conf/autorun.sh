232cf541373f3c68b74375faa56a0380fccc9e14
Alexander Nestorov <alexandernst@gmail.com>
2021-11-11 20:54:42 +0100

    Use random password for Netdata

--- conf/autorun.sh
+++ conf/autorun.sh
@@ -3,6 +3,8 @@
 # WARNING: This script will be executed as the "admin" user.
 # If you want to run it as another user, use su/sudo.
 
+echo "netdata:$(curl -s http://169.254.169.254/latest/meta-data/instance-id | mkpasswd --stdin --method=sha-256)" | sudo tee -a /var/www/webapp/conf/netdata/password
+
 cd /var/www/webapp/src
 
 # Install some dependencies
